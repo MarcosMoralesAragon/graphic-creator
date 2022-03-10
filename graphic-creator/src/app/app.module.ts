02acd2dd39b7dea68c6c6710d4b200e9a4a93c75
Marcos Morales Aragon <marcosmoralesaragon@gmail.com>
2022-03-10 14:16:30 +0100

    Finish

--- graphic-creator/src/app/app.module.ts
+++ graphic-creator/src/app/app.module.ts
@@ -4,13 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {AccordionModule} from 'primeng/accordion';
+
+import { FormsModule } from '@angular/forms';
+import {PasswordModule} from 'primeng/password';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import { CreateComponent } from './components/create/create.component';
+import { ListComponent } from './components/list/list.component';
+import { GraphicComponent } from './components/graphic/graphic.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    ListComponent,
+    GraphicComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AccordionModule,
+    PasswordModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
